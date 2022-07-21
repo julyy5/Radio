65772b0664a526f994acb16488aba550f74a13fb
julyy5 <smirnova-1999@inbox.ru>
2022-07-21 17:44:39 +0300

    Radio

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class Radio {
+public class Radio {
+    private int currentRadioStation;
+    private int currentVolume;
+
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+    public void setCurrentRadioStation(int currentRadioStation) {
+        if (currentRadioStation < 0) {
+            return;
+        }
+
+        if (currentRadioStation > 9) {
+            return;
+        }
+        this.currentRadioStation = currentRadioStation;
+    }
+
+    public int prev() {
+        if (currentRadioStation > 0) {
+            currentRadioStation = currentRadioStation - 1;
+            return currentRadioStation;
+        }
+
+        return currentRadioStation = 9;
+    }
+
+    public int next() {
+        if (currentRadioStation < 9) {
+            currentRadioStation = currentRadioStation + 1;
+            return currentRadioStation;
+        }
+            return currentRadioStation = 0;
+
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+
+        }
+        if (currentVolume > 10) {
+            return;
+        }
+
+        this.currentVolume = currentVolume;
+    }
+
+    public int increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+        return currentVolume = 10;
+    }
+
+    public int reduceVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+        return currentVolume = 0;
+    }
 }
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -1,2 +1,97 @@
-package PACKAGE_NAME;public class RadioTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+
+    @Test
+
+    public void actualStation() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(3);
+        int expected = 3;
+        int actual = radio.getCurrentRadioStation();
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+
+    public void actualVolume() {
+        Radio volume = new Radio();
+        volume.setCurrentVolume(8);
+        int expected = 8;
+        int actual = volume.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+
+    public void nextStation() {
+        Radio nextStation = new Radio();
+        nextStation.setCurrentRadioStation(2);
+        int expected = 3;
+        int actual = nextStation.next();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void nextStationMore9() {
+        Radio nextStation = new Radio();
+        nextStation.setCurrentRadioStation(9);
+        int expected = 0;
+        int actual = nextStation.next();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+
+    public void prevStation() {
+        Radio prevStation = new Radio();
+        prevStation.setCurrentRadioStation(7);
+        int expected = 6;
+        int actual = prevStation.prev();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void prevStationLess0() {
+        Radio prevStation = new Radio();
+        prevStation.setCurrentRadioStation(0);
+        int expected = 9;
+        int actual = prevStation.prev();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+
+    public void increaseVolume() {
+        Radio incrVolume = new Radio();
+        incrVolume.setCurrentVolume(11);
+        int expected = 10;
+        int actual = incrVolume.increaseVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void reduceVolume() {
+        Radio redVolume = new Radio();
+        redVolume.setCurrentVolume(0);
+        int expected = 0;
+        int actual = redVolume.reduceVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
